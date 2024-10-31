7fec65667c0d03d1a5deee35ef1b31fe56546106
olathegreat <ademolarotimi2000@gmail.com>
2024-10-31 21:44:12 +0100

    feat: crud backend

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,8 @@ import express,{Request, Response} from "express"
 import cors from "cors"
 import "dotenv/config"
 import mongoose from "mongoose";
-
+import {v2 as cloudinary} from "cloudinary" 
+import MyRestaurantRoutes from "./routes/MyRestaurantRoutes"    
 import myUserRoute from "./routes/MyUserRoutes" 
 
 
@@ -14,9 +15,16 @@ mongoose.connect(process.env.MONGO_CONNECTION_STRING as string).then(() => {
     console.log(err);
 })  
 
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,  
+    api_key: process.env.CLOUDINARY_API_KEY,    
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+})
+
 const app = express();
 app.use(express.json())
 app.use(cors()) 
+
 
 
 app.get('/health', async(req:Request, res:Response)=>{
@@ -25,7 +33,8 @@ app.get('/health', async(req:Request, res:Response)=>{
     })
 });
 
-app.use('/api/my/user', myUserRoute)
+app.use('/api/my/user', myUserRoute);
+app.use('/api/my/restaurant', MyRestaurantRoutes);
 
 
 app.listen(7000, () => {
--- src/middleware/validation.ts
+++ src/middleware/validation.ts
@@ -2,8 +2,11 @@ import { Request, Response, NextFunction } from 'express';
 import { body, validationResult } from 'express-validator';
 
 const handleValidationErrors = (req: Request, res: Response, next: NextFunction): void => {
+  
+  console.log(typeof req.body); 
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
+    console.log("error here", errors)
     res.status(400).json({ errors: errors.array() });
     return;
   }
@@ -18,4 +21,36 @@ export const validateMyUserRequest = [
     body('city').isString().notEmpty().withMessage('City must be a string'),  
     handleValidationErrors,  
 
+];
+
+export const validateMyRestaurantRequest =[
+  body('restaurantName').notEmpty().withMessage('Restaurant name is required'), 
+  body('city').notEmpty().withMessage('City is required'),
+  body('country').notEmpty().withMessage('Country is required'),
+  body('deliveryPrice').isFloat({min:0}).withMessage('Delivery price must be a number'),
+  body('estimatedDeliveryTime').isInt({min:0}).withMessage('Estimated delivery time must be a number'),
+  // body('cuisines').isArray().withMessage('Cuisines must be an array').notEmpty(),
+  // body('menuItems').isArray().withMessage('Menu items must be an array').notEmpty(),
+  body('cuisines').custom(value => Array.isArray(value)).withMessage('Cuisines must be an array'),
+body('menuItems').custom(value => {
+    if (typeof value === 'string') {
+        value = JSON.parse(value); // parse JSON string to array of objects
+    }
+    if (!Array.isArray(value)) {
+        throw new Error('Menu items must be an array');
+    }
+    value.forEach(item => {
+        if (typeof item.name !== 'string' || item.name.trim() === '') {
+            throw new Error('Each menu item must have a valid name');
+        }
+        if (isNaN(item.price) || item.price < 0) {
+            throw new Error('Each menu item price must be a non-negative number');
+        }
+    });
+    return true;
+}),
+
+  body('menuItems.*.name').isString().notEmpty().withMessage('Menu item name must be a string and is required'),
+  body('menuItems.*.price').isFloat({min:0}).withMessage('Menu item price must be a number and is requuired'),
+  handleValidationErrors,
 ]
